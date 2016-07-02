49a79f912436d956a67089ed3ea26b88e79d3b86
darthpbal <ronquillo.paul@gmail.com>
2016-07-02 09:50:18 -0700

    added class documentation and corrected the for loop for creating a non existing task file.

--- src/TaskList.cpp
+++ src/TaskList.cpp
@@ -3,30 +3,22 @@
 #include "TaskList.hpp"
 
 using namespace std;
-//string taskFileName = "tasks.src";
 
 TaskList::TaskList(string taskFileName){     //ctor
+  cout << "Task list capacity is " << taskListCapacity << endl;
   //Open task source file
   ifstream taskFile(taskFileName.c_str());
   if(!taskFile) {
     cout << "Unable to open tasks source file. Attempting to create new file...\n";
     taskFile.close();
     ofstream blankFile(taskFileName.c_str());
-    for(int i = taskListCapacity; i < taskListCapacity; i++){
-      // list[i].clear();
-      // list[i].dispContents();
-    	//blankFile << list[i].delim() << endl;
+    for(int i = 0; i < taskListCapacity; i++){
+      // cout << "looping...\n";
+      list[i].clear();
+      list[i].dispContents();
+    	blankFile << list[i].delim() << endl;
     }
     blankFile.close();
     exit(-1);
   }
-  int lineNum = 0;
-  while(!taskFile.eof()){
-    string line;
-    getline(taskFile, line);
-    //makeTask(line, myTasks[lineNum]);
-    list[lineNum].make(line);
-    lineNum++;
-  }
-  taskFile.close();
 }
--- src/TaskList.hpp
+++ src/TaskList.hpp
@@ -7,35 +7,43 @@
 
 using namespace std;
 
+/*
+the Task list class is a class representation of a task list. The way it's
+implemented here, the task list is essentially a task array with helper
+functions for operating on the line item level and for handling file operations.
+
+--------------------------------------
+taskListCapacity    |the number of tasks that will be able to be tracked by the program
+TaskList            | ctor
+saveList            |function for saving the contents of the task array to the task file
+constructList       |construct the task list from the source file.
+getTask             |get task by task number
+updateTask          |update task by number
+createTask          |create a new task
+deleteTask          |delete a task by number
+setTask             |set a task by number
+clearTask           |set a task to empty based on id number
+list                |arra of tasks
+--------------------------------------
+*/
+
 class TaskList{
 public:
+    const static int taskListCapacity = 20;
     TaskList(string);             //ctor
     bool saveList();             //save task list to file.
     bool constructList();
-    string getTask();
-    /*
-    	maketask and mytasks array are not declared and currently being deprecated
-    	while(!taskFile.eof()){
-    	string line;
-    	getline(taskFile, line);
-    	makeTask(line, myTasks[lineNum);
-    OOP list[lineNum].make(line);
-    	lineNum++;
-  }
-    */
 
-    bool updateTask(/*task data*/);
-    bool createTask(/*task data*/);
+    string getTask(int taskNum);
+    bool updateTask(int taskNum, string taskLine);
+    bool createTask(string taskLine);
     bool deleteTask(int taskNum);
 
-    bool setTask(int taskNum);   //set task based on task number
+    bool setTask(int taskNum, string taskLine);   //set task based on task number
     bool clearTask(int taskNum); //clear task by task number
-    const static int taskListCapacity = 20;
 
 private:
     Task list[taskListCapacity];
 };
 
 #endif // TASKLIST_HPP
-
-
